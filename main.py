dc2cebe476e3345b145bd0cb7b3d6cbbb3d734a2
SVInaySai1 <visaliveru@pagoanalytics.com>
2024-06-07 13:49:19 +0530

    user

--- main.py
+++ main.py
@@ -10,61 +10,61 @@ from routes.basic_info_form import create_basic_info, get_basic_info, update_bas
 from routes.company import create_company, get_company, update_company, delete_company
 from routes.location_form import create_location, get_location, update_location, delete_location
 from routes.profile_form import create_profile, get_profile, update_profile, delete_profile
-from routes.registration_form import create_user, get_user, update_user, delete_user
+from routes.registration_form import create_user as create_registration_user, get_user as get_registration_user, update_user as update_registration_user, delete_user as delete_registration_user
 from routes.team_form import create_team, get_team, update_team, delete_team
-#from routes.user_form import create_user, get_user, update_user, delete_user
-
+from routes.team_user_form import create_user as create_team_user, get_user as get_team_user, update_user as update_team_user, delete_user as delete_team_user
 
 app = Flask(__name__)
 CORS(app)
 
 # basic info form 
-app.route('/create_basic_info', methods=['POST'])(create_basic_info)
-app.route('/get_basic_info', methods=['GET'])(get_basic_info)
-app.route('/update_basic_info', methods=['PUT'])(update_basic_info)
-app.route('/delete_basic_info', methods=['DELETE'])(delete_basic_info)
+app.route('/create_basic_info', methods=['POST'], endpoint='create_basic_info')(create_basic_info)
+app.route('/get_basic_info', methods=['GET'], endpoint='get_basic_info')(get_basic_info)
+app.route('/update_basic_info', methods=['PUT'], endpoint='update_basic_info')(update_basic_info)
+app.route('/delete_basic_info', methods=['DELETE'], endpoint='delete_basic_info')(delete_basic_info)
 
-#company
-app.route('/create_company', methods=['POST'])(create_company)
-app.route('/get_company', methods=['GET'])(get_company)
-app.route('/update_company', methods=['PUT'])(update_company)
-app.route('/delete_company', methods=['DELETE'])(delete_company)
+# company
+app.route('/create_company', methods=['POST'], endpoint='create_company')(create_company)
+app.route('/get_company', methods=['GET'], endpoint='get_company')(get_company)
+app.route('/update_company', methods=['PUT'], endpoint='update_company')(update_company)
+app.route('/delete_company', methods=['DELETE'], endpoint='delete_company')(delete_company)
 
-#location
-app.route('/create_location', methods=['POST'])(create_location)
-app.route('/get_location', methods=['GET'])(get_location)
-app.route('/update_location', methods=['PUT'])(update_location)
-app.route('/delete_location', methods=['DELETE'])(delete_location)
+# location
+app.route('/create_location', methods=['POST'], endpoint='create_location')(create_location)
+app.route('/get_location', methods=['GET'], endpoint='get_location')(get_location)
+app.route('/update_location', methods=['PUT'], endpoint='update_location')(update_location)
+app.route('/delete_location', methods=['DELETE'], endpoint='delete_location')(delete_location)
 
-#profile form
-app.route('/create_profile', methods=['POST'])(create_profile)
-app.route('/get_profile', methods=['GET'])(get_profile)
-app.route('/update_profile', methods=['PUT'])(update_profile)
-app.route('/delete_profile', methods=['DELETE'])(delete_profile)
+# profile form
+app.route('/create_profile', methods=['POST'], endpoint='create_profile')(create_profile)
+app.route('/get_profile', methods=['GET'], endpoint='get_profile')(get_profile)
+app.route('/update_profile', methods=['PUT'], endpoint='update_profile')(update_profile)
+app.route('/delete_profile', methods=['DELETE'], endpoint='delete_profile')(delete_profile)
 
-#team form
-app.route('/create_team', methods=['POST'])(create_team)
-app.route('/get_team', methods=['GET'])(get_team)
-app.route('/update_team', methods=['PUT'])(update_team)
-app.route('/delete_team', methods=['DELETE'])(delete_team)
+# team form
+app.route('/create_team', methods=['POST'], endpoint='create_team')(create_team)
+app.route('/get_team', methods=['GET'], endpoint='get_team')(get_team)
+app.route('/update_team', methods=['PUT'], endpoint='update_team')(update_team)
+app.route('/delete_team', methods=['DELETE'], endpoint='delete_team')(delete_team)
 
+# registration form CRUD operations
+app.route('/create_registration_user', methods=['POST'], endpoint='create_registration_user')(create_registration_user)
+app.route('/get_registration_user', methods=['GET'], endpoint='get_registration_user')(get_registration_user)
+app.route('/update_registration_user', methods=['PUT'], endpoint='update_registration_user')(update_registration_user)
+app.route('/delete_registration_user', methods=['DELETE'], endpoint='delete_registration_user')(delete_registration_user)
 
-#registration form crud operations
-app.route('/create_user', methods=['POST'])(create_user)
-app.route('/get_user', methods=['GET'])(get_user)
-app.route('/update_user', methods=['PUT'])(update_user)
-app.route('/delete_user', methods=['DELETE'])(delete_user)
+# team user form CRUD operations
+app.route('/create_team_user', methods=['POST'], endpoint='create_team_user')(create_team_user)
+app.route('/get_team_user', methods=['GET'], endpoint='get_team_user')(get_team_user)
+app.route('/update_team_user', methods=['PUT'], endpoint='update_team_user')(update_team_user)
+app.route('/delete_team_user', methods=['DELETE'], endpoint='delete_team_user')(delete_team_user)
 
 # login 
-app.route('/login', methods=['POST'])(login)
+app.route('/login', methods=['POST'], endpoint='login')(login)
 
-
-#email generation
-app.route('/send-otp', methods=['POST'])(send_otp_route)
-app.route('/verify-otp', methods=['POST'])(verify_otp_route)
-
-
-
+# email OTP generation
+app.route('/send-otp', methods=['POST'], endpoint='send_otp')(send_otp_route)
+app.route('/verify-otp', methods=['POST'], endpoint='verify_otp')(verify_otp_route)
 
 if __name__ == '__main__':
     initialize_database()
